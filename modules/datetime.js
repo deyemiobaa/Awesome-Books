2afb1be6a66ceb37dfdeedfd9ddd0d6cb40ff64d
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-03-21 08:12:04 +0100

    fix style errors

--- modules/datetime.js
+++ modules/datetime.js
@@ -5,7 +5,7 @@ const Date = () => {
   const currentTime = DateTime.local();
 
   dateContainer.textContent = `${currentTime.monthLong} ${currentTime.day} ${currentTime.year}, ${currentTime.hour}:${currentTime.minute}`;
-  setTimeout(() => { Date(); }, 1000)
+  setTimeout(() => { Date(); }, 1000);
 };
 
 export default Date;
